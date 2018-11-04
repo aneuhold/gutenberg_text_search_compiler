4836e35c65a66dc9390e43d16b3dce05c1f55e63
Daniel Evans <evans.daniel.n@gmail.com>
2018-11-04 13:50:31 -0800

    Working\?

--- aggregate.rb
+++ aggregate.rb
@@ -7,39 +7,100 @@ require 'benchmark'
 require 'pp'
 require 'pry'
 require 'progressbar'
+require 'optionparser'
 
+class Aggregator
+  attr_accessor :directories
 
-results = Hash.new do |h,k|
-  h[k] = []
-end
-files = Dir.glob("results/**/*.json")
+  def initialize
+    self.directories = ["results"]
+  end
 
-progress = ProgressBar.create(title: "Files", total: files.length)
-
-rt = Benchmark.realtime do
-  files.each do |file|
-    file_data = nil
-    File.open(file, 'rb') do |f|
-      file_data = f.read
+  def aggregate!
+    results = Hash.new do |h,k|
+      h[k] = []
     end
-    file_data = JSON.parse(file_data)
+    files = directories.map do |dir|
+      Dir.glob(File.join(dir, "**/*.json"))
+    end.flatten.uniq
 
-    id = file_data["id"]
-    file_data["paragraphs"].each_with_index do |paragraph, i|
-      paragraph["words"].each do |word, count|
-        $stderr.puts "count > 99!" if count > 99
-        results[word].push sprintf("%02d:%s:%d", count, id, i)
+    progress = ProgressBar.create(title: "Files", total: files.length, format: "%t (%c/%C): %w")
+
+    rt = Benchmark.realtime do
+      files.each do |file|
+        file_data = nil
+        File.open(file, 'rb') do |f|
+          file_data = f.read
+        end
+        file_data = JSON.parse(file_data)
+
+        id = file_data["id"]
+        file_data["paragraphs"].each_with_index do |paragraph, i|
+          paragraph["words"].each do |word, count|
+            $stderr.puts "count > 99!" if count > 99
+            results[word].push sprintf("%02d:%s:%d", count, id, i)
+          end
+        end
+
+        progress.increment
+      end
+    end
+    progress.finish
+
+
+    dir = "results_aggregate"
+    FileUtils.mkdir_p dir
+    puts "Completed in #{rt}s"
+    puts "Writing File"
+    results.each_value(&:sort!)
+    rt = Benchmark.realtime do
+      json = JSON.generate(results)
+      File.open(File.join(dir, "db.json"), "wb") do |f|
+        f.write(json)
       end
     end
 
-    progress.increment
+
+    puts "Writing Word list"
+    rt = Benchmark.realtime do
+      File.open(File.join(dir, "words.txt"), "wb") do |f|
+        f.write(results.keys.join("\n"))
+      end
+    end
+    puts "Completed in #{rt}s"
+
+
+    puts "Writing Letter Lists"
+    rt = Benchmark.realtime do
+      first_letter = results.group_by { |key, value| key[0] }
+      first_letter.each do |letter, results|
+        json = JSON.generate(results.to_h)
+        File.open(File.join(dir, "#{letter}.json"), "wb") do |f|
+          f.write(json)
+        end
+      end
+    end
+    puts "Completed in #{rt}s"
   end
 end
-progress.finish
-puts "Completed in #{rt}s"
-results.each_value(&:sort!)
 
-json = JSON.pretty_generate(results)
-File.open("db.json", "wb") do |f|
-  f.write(json)
-end
+
+aggregator = Aggregator.new
+
+
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: parse.rb [options]"
+
+  opts.on("-d=dir", "--directory=dir,dir,dir", Array, "directory to parse") do |v|
+    aggregator.directories = v
+  end
+
+  opts.on("-p", "show progress") do
+    aggregator.progressbar = ProgressBar.create title: "Directories", format: "%t (%c/%C): %w"
+  end
+
+end.parse!
+
+
+aggregator.parse!
--- parse.rb
+++ parse.rb
@@ -8,6 +8,7 @@ require 'pp'
 require 'pry'
 require 'optionparser'
 require 'set'
+require 'progressbar'
 
 STOP_WORDS = [ # ripped off from https://github.com/brez/stopwords/blob/master/lib/stopwords.rb
   'a','cannot','into','our','thus','about','co','is','ours','to','above',
@@ -45,120 +46,164 @@ START_REGEXP = /^\*+\s*START[^$]*PROJECT GUTENBERG[^\*]*\*{3,}\s*$/im
 INITIAL_CHOMP_REGEXP = /\*{3,}[^*]*\*{3,}\s*/
 END_REGEXP = /^\*+\s*END[^$]*PROJECT GUTENBERG/im
 PUNCTUATION_REGEXP = /\p{Punct}+$/
-MINIMUM_WORDS = 10
+MINIMUM_WORDS = 2
 
-def whitelisted_word? word
-  @whitelist ||= begin
-                   if File.exist? "whitelist.txt"
-                     File.read("whitelist.txt").split
-                   else
-                     generate_whitelist
+class Parser
+  attr_accessor :minimum_words
+  attr_accessor :directories
+  attr_accessor :verbose
+  attr_accessor :progressbar
+
+  def initialize dirs="gutenberg_data", min_words=MINIMUM_WORDS
+    self.directories = Array(dirs)
+    self.minimum_words = min_words
+  end
+
+  def whitelisted_word? word
+    @whitelist ||= begin
+                     words = if File.exist? "whitelist.txt"
+                               File.read("whitelist.txt").split
+                             else
+                               generate_whitelist
+                             end
+                     Set.new words
                    end
 
-                   @whitelist = Set.new @whitelist
-                 end
-
-end
-
-def generate_whitelist
-  @whitelist = (File.readlines("/usr/share/dict/words").map { |word| word.chomp.downcase.stem } - STOP_WORDS).uniq
-  File.open("whitelist.txt", "wb") do |f|
-    f.write @whitelist.join("\n")
-  end
-end
-
-def unzip f
-  %x{unzip #{Shellwords.shellescape(f)}}
-end
-
-def cleanup
-  Dir.glob("*.txt").each do |f|
-    FileUtils.rm f
+    @whitelist.include? word
   end
 
-  Dir.glob("*").each do |f|
-    FileUtils.rm_r f if File.directory? f
-  end
-end
-
-def paragraphs text
-  aggregator_hash = Hash.new { |h, k| h[k] = 0 }
-
-  text.split(BLANK_LINE_REGEXP).map do |paragraph|
-    words = paragraph.split.each_with_object(aggregator_hash.dup) do |word, memo|
-      sanitary_word = word.gsub(PUNCTUATION_REGEXP, '').downcase
-      memo[sanitary_word.stem] += 1 if whitelisted_word?(sanitary_word)
+  def generate_whitelist
+    words = (File.readlines("/usr/share/dict/words").map { |word| word.chomp.downcase.stem } - STOP_WORDS).uniq
+    File.open("whitelist.txt", "wb") do |f|
+      f.write @whitelist.join("\n")
     end
-    if words.length > MINIMUM_WORDS
-      { text: paragraph, words: words }
-    end
-  end.compact
-end
-
-def write_result dir, results_dir, result
-  destination_dir = File.join(results_dir, dir)
-  FileUtils.mkdir_p destination_dir
-  id = File.split(dir).last
-  json = JSON.generate({ id: id, paragraphs: result })
-  File.open(File.join(destination_dir, "#{id}.json"), "wb") do |f|
-    f.write(json)
+    words
   end
-end
 
-def get_textfile dir
-  file = Dir.glob("*.zip").sort.first
-  return nil if !file
-  unzip file
-  Dir.glob("**/*.txt").first
-end
+  def unzip f
+    %x{unzip #{Shellwords.shellescape(f)}}
+  end
 
-def process_directory dir
-  puts "Processing: #{dir}"
-  rt = Benchmark.realtime do
-    results_dir = File.expand_path('./results')
-    Dir.chdir dir do
-      textfile = get_textfile dir
+  def cleanup
+    Dir.glob("*.txt").each do |f|
+      FileUtils.rm f
+    end
 
-      if !textfile
-        $stderr.puts "Error getting textfile for #{dir}"
-        return false
-      end
-
-      file_data = nil
-
-      File.open(textfile, 'rb') do |f|
-        file_data = f.read
-      end
-
-      start_index = file_data.index START_REGEXP
-      end_index = file_data.index END_REGEXP
-
-      if start_index.nil? || end_index.nil?
-        $stderr.puts "#{File.join(dir, textfile)} did not match expected regular expressions (s:#{start_index} e:#{end_index})"
-        return false
-      end
-
-      file_data = file_data[start_index..end_index].gsub(INITIAL_CHOMP_REGEXP, '').encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
-      if file_data.length > 0
-        write_result dir, results_dir, paragraphs(file_data)
-      else
-        $stderr.puts "#{File.join(dir, textfile)} contains no data"
-      end
-    ensure
-      cleanup
+    Dir.glob("*").each do |f|
+      FileUtils.rm_r f if File.directory? f
     end
   end
-  puts "Complete: #{dir} #{rt}s"
-end
 
-rt = Benchmark.realtime do
-  whitelisted_word? "foo"
-  all_files = Dir.glob(File.join("gutenberg_data", "**/*.zip"))
-  files_by_dir = all_files.group_by { |f| f.gsub(/(.*)\/[^\/]*/, "\\1") }
-  puts "#{files_by_dir.keys.length} directories detected"
-  files_by_dir.each_key.sort.each do |dir|
-    process_directory dir
+  def paragraphs text
+    aggregator_hash = Hash.new { |h, k| h[k] = 0 }
+
+    text.split(BLANK_LINE_REGEXP).map do |paragraph|
+      words = paragraph.split.each_with_object(aggregator_hash.dup) do |word, memo|
+        sanitary_word = word.gsub(PUNCTUATION_REGEXP, '').downcase.stem
+        memo[sanitary_word] += 1 if whitelisted_word?(sanitary_word)
+      end
+      if words.length >= self.minimum_words
+        { text: paragraph, words: words }
+      end
+    end.compact
+  end
+
+  def write_result dir, results_dir, result
+    destination_dir = File.join(results_dir, dir)
+    FileUtils.mkdir_p destination_dir
+    id = File.split(dir).last
+    json = JSON.generate({ id: id, paragraphs: result })
+    File.open(File.join(destination_dir, "#{id}.json"), "wb") do |f|
+      f.write(json)
+    end
+  end
+
+  def get_textfile dir
+    file = Dir.glob("*.zip").sort.first
+    return nil if !file
+    unzip file
+    Dir.glob("**/*.txt").first
+  end
+
+  def process_directory dir
+    puts "Processing: #{dir}" if verbose
+    rt = Benchmark.realtime do
+      results_dir = File.expand_path('./results')
+      Dir.chdir dir do
+        textfile = get_textfile dir
+
+        if !textfile
+          $stderr.puts "Error getting textfile for #{dir}"
+          return false
+        end
+
+        file_data = nil
+
+        File.open(textfile, 'rb') do |f|
+          file_data = f.read
+        end
+
+        start_index = file_data.index START_REGEXP
+        end_index = file_data.index END_REGEXP
+
+        if start_index.nil? || end_index.nil?
+          $stderr.puts "#{File.join(dir, textfile)} did not match expected regular expressions (s:#{start_index} e:#{end_index})"
+          return false
+        end
+
+        file_data = file_data[start_index..end_index].gsub(INITIAL_CHOMP_REGEXP, '').encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
+        if file_data.length > 0
+          write_result dir, results_dir, paragraphs(file_data)
+        else
+          $stderr.puts "#{File.join(dir, textfile)} contains no data"
+        end
+      ensure
+        cleanup
+      end
+    end
+    puts "Complete: #{dir} #{rt}s" if verbose
+  end
+
+  def parse!
+    count = 1
+    rt = Benchmark.realtime do
+      whitelisted_word? "foo" # generates the whitelist in the right place
+
+      all_files = directories.map do |dir|
+        Dir.glob(File.join(dir, "**/*.zip"))
+      end.flatten.uniq
+
+      files_by_dir = all_files.group_by { |f| f.gsub(/(.*)\/[^\/]*/, "\\1") }
+      count = files_by_dir.length
+      puts "#{count} directories detected"
+      progressbar.total = count if progressbar
+      files_by_dir.each_key.sort.each do |dir|
+        process_directory dir
+        progressbar.increment if progressbar
+      end
+    end
+
+    puts "Complete in #{rt}s"
   end
 end
 
-puts "Complete in #{rt}s"
+parser = Parser.new
+OptionParser.new do |opts|
+  opts.banner = "Usage: parse.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-d=dir", "--directory=dir,dir,dir", Array, "directory to parse") do |v|
+    parser.directories = v
+  end
+
+  opts.on("-p", "show progress") do
+    parser.progressbar = ProgressBar.create title: "Directories", format: "%t (%c/%C): %w"
+  end
+
+end.parse!
+
+
+parser.parse!
